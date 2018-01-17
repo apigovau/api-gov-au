c5de463737b4891affc2cfba9b19174a80a2c23f
colugo <alastair.parker@gmail.com>
2018-01-18 10:58:18 +1100

    Simplify navigation to H2 only, but allow H2-> in detail

--- src/main/kotlin/au/gov/dxa/Controller.kt
+++ src/main/kotlin/au/gov/dxa/Controller.kt
@@ -24,33 +24,27 @@ class Controller {
 
     @RequestMapping("/service/{id}")
     fun detail(@PathVariable id:String, model:MutableMap<String,Any?>): String{
-        return detail_page(id, "", model)
+        return detailPage(id, "", model)
     }
 
 
 
     @RequestMapping("/service/{id}/{title}")
-    fun detail_page(@PathVariable id:String,@PathVariable title:String, model:MutableMap<String, Any?>): String{
-        var serviceDescription = serviceDescriptionService.get(id)
+    fun detailPage(@PathVariable id:String,@PathVariable title:String, model:MutableMap<String, Any?>): String{
+        val serviceDescription = serviceDescriptionService.get(id)
         if(serviceDescription == null) return "detail"
 
         var page:ServiceDescriptionPage = serviceDescription.subpages[0]
 
-        if(title != "") for(loopPage in serviceDescription!!.subpages) {
+        if(title != "") for(loopPage in serviceDescription.subpages) {
             if(title == loopPage.title) {
                 page = loopPage
                 break
             }
         }
-        var rawContent = ""
 
-        rawContent += pageWithHeading(page)
-        if(page.subpages != null) for(subpage in page.subpages as List<ServiceDescriptionPage>) {
-            rawContent += subpageWithHeading(subpage)
-        }
+        val rawContent = getPageData( page, 1)
 
-
-        println(rawContent)
         val content = getMarkdown(rawContent)
 
         model.put("model", serviceDescription)
@@ -58,17 +52,21 @@ class Controller {
         return "detail"
     }
 
-    private fun pageWithHeading(page:ServiceDescriptionPage): String {
+    private fun getPageData(page: ServiceDescriptionPage, indent:Int): String {
+        var rawContent = ""
+        rawContent += pageWithHeading(page, indent)
+        if (page.subpages != null) for (subpage in page.subpages) {
+            rawContent += getPageData(subpage, indent + 1)
+        }
+        return rawContent
+    }
+
+    private fun pageWithHeading(page:ServiceDescriptionPage, indent:Int): String {
         var output = "\n<a name=\"${page.title}\"></a>\n\n"
-        output += "# ${page.title}\n\n${page.content}"
+        output += "${"#".repeat(indent)} ${page.title}\n\n${page.content}"
         return output
     }
 
-    private fun subpageWithHeading(page:ServiceDescriptionPage): String {
-        var output = "\n<a name=\"${page.title}\"></a>\n\n"
-        output += "## ${page.title}\n\n${page.content}"
-        return output
-    }
 
     private fun getMarkdown(md:String):String{
         val options = MutableDataSet()
