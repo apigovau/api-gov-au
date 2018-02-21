f6ba0f2f8ca5d72d81615523d5fe7316a0150e9d
colugo <alastair.parker@gmail.com>
2018-02-22 09:38:06 +1100

    redirect http to https

---
+++ src/main/kotlin/au/gov/dxa/SecurityConfig.kt
@@ -0,0 +1,16 @@
+package au.gov.dxa
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@Profile("prod")
+open class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.requiresChannel().anyRequest().requiresSecure()
+    }
+}
