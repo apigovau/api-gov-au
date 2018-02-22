a512b674796f4710b764c10132848bc31ebb650b
colugo <alastair.parker@gmail.com>
2018-02-23 08:31:41 +1100

    CORS header for /swagger/

--- src/main/kotlin/au/gov/dxa/Application.kt
+++ src/main/kotlin/au/gov/dxa/Application.kt
@@ -2,6 +2,10 @@ package au.gov.dxa
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 open class Application {
@@ -9,6 +13,16 @@ open class Application {
         @JvmStatic fun main(args: Array<String>) {
             SpringApplication.run(Application::class.java, *args)
         }
+
+        @Bean
+        fun corsConfigurer(): WebMvcConfigurer {
+            return object : WebMvcConfigurerAdapter() {
+                override fun addCorsMappings(registry: CorsRegistry?) {
+                    registry!!.addMapping("/swagger/*").allowedOrigins("*")
+                }
+            }
+        }
+
     }
 
 }
