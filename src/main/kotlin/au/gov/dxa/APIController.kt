58b51e6762355321a29ff01e45a673b8a4e30798
colugo <alastair.parker@gmail.com>
2018-04-17 13:48:49 +1000

    Working feedback

--- src/main/kotlin/au/gov/dxa/APIController.kt
+++ src/main/kotlin/au/gov/dxa/APIController.kt
@@ -1,16 +1,41 @@
 
 package au.gov.dxa
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.sql.Connection
+import java.sql.SQLException
+import javax.sql.DataSource
 
 @RestController
 class APIController {
 
-    @GetMapping("/feedback")
-    fun feedback(@RequestParam path:String, @RequestParam score:Int){
-        System.out.println("Page: '${path}' got score: ${score}")
+
+    @Autowired
+    private lateinit var feedback: Feedback
+
+    @GetMapping("/api/feedback")
+    fun feedback(@RequestParam path:String, @RequestParam upVotes:Int):String{
+        return feedback.feedback(path, upVotes)
     }
+
+    @GetMapping("/api/allFeedback")
+    fun getFeedback(): Feedback.Feedback {
+        return feedback.getFeedback()
+    }
+
+    @GetMapping("/api/pathFeedback")
+    fun getFeedback(@RequestParam path:String): String {
+        return feedback.getFeedback(path)
+    }
+
 }
